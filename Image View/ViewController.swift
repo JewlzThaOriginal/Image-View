0815ae5d567a24a42fc19901acbd1febb44f23a5
Aiden Martinez <Angrybull2007@gmail.com>
2022-01-24 14:14:37 -0700

    Finished Amgus

--- Image View/ViewController.swift
+++ Image View/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var Amgus: UIImageView!
+    let myImage = UIImage (named:  "Amgus")
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        super.viewDidLoad()
+        
+        Amgus.image = myImage
     }
 
-
+    @IBAction func AmogChanger(_ sender: UIButton) {
+        let myImage = UIImage (named: "Drip Jacket")
+        Amgus.image = myImage
+        
+        
+        
+        
+        
+    }
+    
 }
 
